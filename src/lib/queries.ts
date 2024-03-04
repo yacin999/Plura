edccbcefed42a474d3a08f0f1edac13739446b87
yacine <yacinomar999@gmail.com>
2024-03-04 20:22:18 +0100

    accept invitations

--- src/lib/queries.ts
+++ src/lib/queries.ts
@@ -3,6 +3,7 @@
 import { currentUser } from "@clerk/nextjs"
 import { db } from "./db"
 import { redirect } from "next/navigation"
+import { User } from "@prisma/client"
 
 export const getAuthUserDetails = async ()=>{
     const user = await currentUser()
@@ -32,13 +33,89 @@ export const getAuthUserDetails = async ()=>{
     return userData
 }
 
+
+const saveActivityLogsNotifications = async ({
+    agencyId,
+    description,
+    subAccountId
+} : {
+    agencyId? : string,
+    description : string,
+    subAccountId? : string
+})=>{
+    const authUser = await currentUser()
+    let userData;
+    if (!authUser){
+        const response = await db.user.findFirst({
+            where : {
+                Agency : {
+                    SubAccount : {
+                        some : {id : subAccountId}
+                    }
+                }
+            }
+        })
+        if(response) {
+            userData = response
+        }
+    }else {
+        userData = await db.user.findUnique({
+            where : {
+                email : authUser?.emailAddresses[0].emailAddress,
+            }
+        })
+    }
+
+    if(!userData) {
+        console.log("could not find a user !!")
+        return 
+    }
+
+    let foundAgencyId = agencyId
+    if (!foundAgencyId) {
+        if (!subAccountId) {
+            throw new Error("you need to provide atleast an agency Id or subaccount Id")
+        }
+    }
+
+    const response = await db.subAccount.findUnique({
+        where : {id : subAccountId}
+    })
+
+    if (response) foundAgencyId = response.agencyId
+
+}
+
+
+const createTeamUser = async(agencyId : string, user : User)=> {
+    if (user.role === 'AGENCY_OWNER') return null
+    const response = await db.user.create({data : {...user}})
+    return response
+}
+
+
+
 export const verifyAndAcceptInvitation = async ()=>{
     const user = await currentUser()
     if(!user) return redirect("/sign-in")
     const invitationExists = await db.invitation.findUnique({
         where : {
-            email : user.emailAddresses[0].emailAddress
+            email : user.emailAddresses[0].emailAddress,
+            status : 'PENDING'
         }
     })
+
+    if (invitationExists) {
+        const userDetails = await createTeamUser(invitationExists.agencyId, {
+            email : invitationExists.email,
+            agencyId : invitationExists.agencyId,
+            avatarUrl : user.imageUrl,
+            id : user.id,
+            name : `${user.firstName} ${user.lastName}`,
+            role : invitationExists.role,
+            createdAt : new Date(),
+            updatedAt : new Date()
+        })
+    }
 
 }
