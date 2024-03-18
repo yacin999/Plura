cb8485140c119bb46a29fcea50ed3063ab4379bf
yacine <yacinomar999@gmail.com>
2024-03-18 12:57:44 +0100

    create team table

--- src/lib/queries.ts
+++ src/lib/queries.ts
@@ -453,3 +453,30 @@ export const deleteSubAccount = async (subaccountId : string) =>{
 
   return response
 }
+
+// delete a user by its Id
+export const deleteUser = async (userId: string) => {
+  await clerkClient.users.updateUserMetadata(userId, {
+    privateMetadata: {
+      role: undefined,
+    },
+  })
+  const deletedUser = await db.user.delete({ where: { id: userId } })
+
+  return deletedUser
+}
+
+
+
+
+
+// get a user by id
+export const getUser = async (id: string) => {
+  const user = await db.user.findUnique({
+    where: {
+      id,
+    },
+  })
+
+  return user
+}
