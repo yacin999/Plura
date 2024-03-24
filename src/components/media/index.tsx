fb4cc580b9455b9fe8f4fce274d474f29d960f54
yacine <yacinomar999@gmail.com>
2024-03-24 14:23:59 +0100

    fix a bug in media page

--- src/components/media/index.tsx
+++ src/components/media/index.tsx
@@ -1,7 +1,7 @@
 import { GetMediaFiles } from '@/lib/types'
 import React from 'react'
 import MediaUploadButton from './upload-buttons'
-import { Command, CommandEmpty, CommandInput, CommandItem, CommandList } from '../ui/command'
+import { Command, CommandEmpty, CommandGroup, CommandInput, CommandItem, CommandList } from '../ui/command'
 import MediaCard from './media-card'
 import { FolderSearch } from 'lucide-react'
 
@@ -21,25 +21,27 @@ const MediaComponent = ({data, subaccountId}: Props) => {
           <CommandInput placeholder='Search for file name...'/>
           <CommandList className='pb-40 max-h-full'>
             <CommandEmpty>No media files</CommandEmpty>
-            <div className='flex flex-wrap gap-4 pt-4'>
-              {data?.Media.map(file=>(
-                <CommandItem 
-                  key={file.id}
-                  className='p-0 max-w-[300px] w-full rounded-lg !bg-transparent !font-medium !text-white'
-                >
-                  <MediaCard file={file}/>
-                </CommandItem>
-              ))}
-              {!data?.Media.length && (
-                <div className='flex items-center justify-center w-full flex-col'>
-                  <FolderSearch
-                    size={200}
-                    className='dark:text-muted text-slate-300'
-                  />
-                  <p className='text-muted-foreground'>Empty! no files to show.</p>
-                </div>
-              )}
-            </div>
+            <CommandGroup heading='Media Files'>
+              <div className='flex flex-wrap gap-4 pt-4'>
+                {data?.Media.map(file=>(
+                  <CommandItem 
+                    key={file.id}
+                    className='p-0 max-w-[300px] w-full rounded-lg !bg-transparent !font-medium !text-white'
+                  >
+                    <MediaCard file={file}/>
+                  </CommandItem>
+                ))}
+                {!data?.Media.length && (
+                  <div className='flex items-center justify-center w-full flex-col'>
+                    <FolderSearch
+                      size={200}
+                      className='dark:text-muted text-slate-300'
+                    />
+                    <p className='text-muted-foreground'>Empty! no files to show.</p>
+                  </div>
+                )}
+              </div>
+            </CommandGroup>
           </CommandList>
         </Command>
     </div>
