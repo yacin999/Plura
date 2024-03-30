f751550f34cce6d70dc2d143130ab0cdaa2508cb
yacine <yacinomar999@gmail.com>
2024-03-30 18:24:44 +0100

    complete ticket form

--- src/components/forms/ticket-form.tsx
+++ src/components/forms/ticket-form.tsx
@@ -12,6 +12,17 @@ import { z } from 'zod'
 import { Card, CardContent, CardHeader, CardTitle } from '../ui/card'
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '../ui/form'
 import { Input } from '../ui/input'
+import { Textarea } from '../ui/textarea'
+import { Select } from '@radix-ui/react-select'
+import { SelectContent, SelectItem, SelectTrigger, SelectValue } from '../ui/select'
+import { Avatar, AvatarFallback, AvatarImage } from '../ui/avatar'
+import { CheckIcon, ChevronsUpDownIcon, User2 } from 'lucide-react'
+import { Popover, PopoverContent, PopoverTrigger } from '../ui/popover'
+import { Button } from '../ui/button'
+import { Command, CommandGroup, CommandInput, CommandItem } from '../ui/command'
+import { CommandEmpty } from 'cmdk'
+import { cn } from '@/lib/utils'
+import Loading from '../global/loading'
 
 type Props = {
     laneId : string,
@@ -122,6 +133,164 @@ const TicketForm = ({laneId, subaccountId, getNewTicket}: Props) => {
                             </FormItem>
                         )}
                     />
+                    <FormField
+                        disabled={isLoading}
+                        control={form.control}
+                        name="description"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Description</FormLabel>
+                                <FormControl>
+                                    <Textarea
+                                        placeholder="Description"
+                                        {...field}
+                                    />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                    <FormField
+                        disabled={isLoading}
+                        control={form.control}
+                        name="value"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Ticket Value</FormLabel>
+                                <FormControl>
+                                    <Input
+                                    placeholder="Value"
+                                    {...field}
+                                    />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                    <h3>Add tags</h3>
+                    {/* <TagCreator
+                        subAccountId={subaccountId}
+                        getSelectedTags={setTags}
+                        defaultTags={defaultData.ticket?.Tags || []}
+                    />   */}
+                    <FormLabel>Assigned to Team Member</FormLabel>
+                    <Select
+                        onValueChange={setAssignedTo}
+                        defaultValue={assignedTo}
+                    >
+                        <SelectTrigger>
+                            <SelectValue
+                                placeholder={
+                                    <div className="flex items-center gap-2">
+                                        <Avatar className="w-8 h-8">
+                                            <AvatarImage alt="contact" />
+                                            <AvatarFallback className="bg-primary text-sm text-white">
+                                                <User2 size={14} />
+                                            </AvatarFallback>
+                                        </Avatar>
+
+                                        <span className="text-sm text-muted-foreground">
+                                            Not Assigned
+                                        </span>
+                                    </div>
+                                }
+                            />
+                        </SelectTrigger>
+                        <SelectContent>
+                            {allTeamMembers.map((teamMember) => (
+                                <SelectItem
+                                    key={teamMember.id}
+                                    value={teamMember.id}
+                                >
+                                    <div className="flex items-center gap-2">
+                                    <Avatar className="w-8 h-8">
+                                        <AvatarImage
+                                        alt="contact"
+                                        src={teamMember.avatarUrl}
+                                        />
+                                        <AvatarFallback className="bg-primary text-sm text-white">
+                                            <User2 size={14} />
+                                        </AvatarFallback>
+                                    </Avatar>
+
+                                    <span className="text-sm text-muted-foreground">
+                                        {teamMember.name}
+                                    </span>
+                                    </div>
+                                </SelectItem>
+                            ))}
+                        </SelectContent>                                
+                    </Select>
+                    <FormLabel>Customer</FormLabel>
+                    <Popover>
+                        <PopoverTrigger
+                            asChild
+                            className="w-full"
+                        >
+                            <Button
+                            variant="outline"
+                            role="combobox"
+                            className="justify-between"
+                            >
+                            {contact
+                                ? contactList.find((c) => c.id === contact)?.name
+                                : 'Select Customer...'}
+                            <ChevronsUpDownIcon className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+                            </Button>
+                        </PopoverTrigger>
+                        <PopoverContent className="w-[400px] p-0">
+                            <Command>
+                            <CommandInput
+                                placeholder="Search..."
+                                className="h-9"
+                                value={search}
+                                onChangeCapture={async (value) => {
+                                    //@ts-ignore
+                                    setSearch(value.target.value)
+                                    if (saveTimerRef.current)
+                                        clearTimeout(saveTimerRef.current)
+                                    saveTimerRef.current = setTimeout(async () => {
+                                        const response = await searchContacts(
+                                        //@ts-ignore
+                                        value.target.value
+                                        )
+                                        setContactList(response)
+                                        setSearch('')
+                                    }, 1000)
+                                }}
+                            />
+                            <CommandEmpty>No Customer found.</CommandEmpty>
+                            <CommandGroup>
+                                {contactList.map((c) => (
+                                <CommandItem
+                                    key={c.id}
+                                    value={c.id}
+                                    onSelect={(currentValue) => {
+                                    setContact(
+                                        currentValue === contact ? '' : currentValue
+                                    )
+                                    }}
+                                >
+                                    {c.name}
+                                    <CheckIcon
+                                    className={cn(
+                                        'ml-auto h-4 w-4',
+                                        contact === c.id ? 'opacity-100' : 'opacity-0'
+                                    )}
+                                    />
+                                </CommandItem>
+                                ))}
+                            </CommandGroup>
+                            </Command>
+                        </PopoverContent>
+                    </Popover>
+                    <Button
+                        className="w-20 mt-4"
+                        disabled={isLoading}
+                        type="submit"
+                    >
+                        {form.formState.isSubmitting ? <Loading /> : 'Save'}
+                    </Button>
                 </form>
             </Form>
         </CardContent>
