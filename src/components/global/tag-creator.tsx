3c697a88c4892e2cfe165967fa9c19e2458308e7
yacine <yacinomar999@gmail.com>
2024-04-02 18:56:27 +0100

    add tag to tag creator

--- src/components/global/tag-creator.tsx
+++ src/components/global/tag-creator.tsx
@@ -5,6 +5,8 @@ import { useRouter } from 'next/navigation'
 import React, { useEffect, useState } from 'react'
 import { AlertDialog } from '../ui/alert-dialog'
 import { Command } from '../ui/command'
+import TagComponent from './tag'
+import { X } from 'lucide-react'
 
 type Props = {
     subAccountId : string,
@@ -26,14 +28,42 @@ const TagCreator = ({subAccountId, getSelectedTags, defaultTags}: Props) => {
         getSelectedTags(selectedTags)
     }, [selectedTags])
 
+    const handleDeleteSelection = (tagId:string) => {
+        setSelectedTags(selectedTags.filter(tag=> tag.id !== tagId))
+    }
     return (
     <AlertDialog>
         <Command className='bg-transparent'>
             {!!selectedTags.length && (
                 <div className='flex flex-wrap gap-2 p-2 bg-background border-2 border-border rounded-md'>
-
+                    {selectedTags.map((tag) => (
+                        <div 
+                            key={tag.id}
+                            className='flex items-center'
+                        >
+                            <TagComponent
+                                title={tag.name}
+                                colorName={tag.color}
+                            />
+                            <X
+                                size={14}
+                                className='text-muted-foreground cursor-pointer'
+                                onClick={() => handleDeleteSelection(tag.id)}
+                            />
+                        </div>
+                    ))}
                 </div>
             )}
+            <div className='flex items-center gap-2 my-2'>
+                {TagColors.map(colorName => (
+                    <TagComponent
+                        key={colorName}
+                        selectedColor={setSelectedColor}
+                        title=''
+                        colorName={colorName}
+                    />
+                ))}
+            </div>
         </Command>
     </AlertDialog>
   )
