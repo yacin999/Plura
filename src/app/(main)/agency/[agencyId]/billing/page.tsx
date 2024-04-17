e9fd57b8fe85c8f6e16b97437c18effa2e95b5a5
yacine <yacinomar999@gmail.com>
2024-04-17 17:56:02 +0100

    add all pricing infos

--- src/app/(main)/agency/[agencyId]/billing/page.tsx
+++ src/app/(main)/agency/[agencyId]/billing/page.tsx
@@ -1,85 +1,125 @@
-import { Separator } from '@/components/ui/separator'
+
+import React from 'react'
+import { stripe } from '@/lib/stripe'
 import { addOnProducts, pricingCards } from '@/lib/constants'
 import { db } from '@/lib/db'
-import { stripe } from '@/lib/stripe'
-import React from 'react'
+import { Separator } from '@/components/ui/separator'
 import PricingCard from './_components/pricing-card'
 
+
 type Props = {
-  params : {agencyId: string}
+  params: { agencyId: string }
 }
 
-const page = async ({params}: Props) => {
-
+const page = async ({ params }: Props) => {
+  //CHALLENGE : Create the add on  products
   const addOns = await stripe.products.list({
-    ids : addOnProducts.map(product => product.id),
-    expand : ['data.default_price']
+    ids: addOnProducts.map((product) => product.id),
+    expand: ['data.default_price'],
   })
 
   const agencySubscription = await db.agency.findUnique({
-    where : {
-      id: params.agencyId
+    where: {
+      id: params.agencyId,
     },
-    select : {
-      customerId : true,
-      Subscription : true
-    }
+    select: {
+      customerId: true,
+      Subscription: true,
+    },
   })
 
   const prices = await stripe.prices.list({
-    product : process.env.NEXT_PLURA_PRODUCT_ID,
-    active : true
+    product: process.env.NEXT_PLURA_PRODUCT_ID,
+    active: true,
   })
 
-  const currentPlanDetails = pricingCards.find(c=>c.priceId === agencySubscription?.Subscription?.priceId)
-  
+  const currentPlanDetails = pricingCards.find(
+    (c) => c.priceId === agencySubscription?.Subscription?.priceId
+  )
+
   const charges = await stripe.charges.list({
-    limit : 50,
-    customer : agencySubscription?.customerId
+    limit: 50,
+    customer: agencySubscription?.customerId,
   })
-  
+
   const allCharges = [
-    ...charges.data.map(charge=> ({
-      description : charge.description,
-      id : charge.id,
-      date : `${ new Date(charge.created * 1000 ).toLocaleTimeString()} ${new Date(charge.created * 1000).toLocaleDateString()}`,
-      status : 'Paid',
-      amount : `$${charge.amount / 100}`
-    }))
+    ...charges.data.map((charge) => ({
+      description: charge.description,
+      id: charge.id,
+      date: `${new Date(charge.created * 1000).toLocaleTimeString()} ${new Date(
+        charge.created * 1000
+      ).toLocaleDateString()}`,
+      status: 'Paid',
+      amount: `$${charge.amount / 100}`,
+    })),
   ]
-  
-  
-  
-  
+
   return (
     <>
-      <h1 className='text-4xl p-4'>Billing</h1>
-      <Separator className='mb-6'/>   
-      <h2 className='text-2xl p-4'>Current Plan</h2>
-      <div className='flex flex-col lg:!flex-row justify-between gap-8'>
-        <PricingCard 
+
+      <h1 className="text-4xl p-4">Billing</h1>
+      <Separator className=" mb-6" />
+      <h2 className="text-2xl p-4">Current Plan</h2>
+      <div className="flex flex-col lg:!flex-row justify-between gap-8">
+        <PricingCard
           planExists={agencySubscription?.Subscription?.active === true}
           prices={prices.data}
-          customerId={agencySubscription?.customerId || ""}
+          customerId={agencySubscription?.customerId || ''}
           amt={
-            agencySubscription?.Subscription?.active === true ? currentPlanDetails?.price || "$0" : "$0"
+            agencySubscription?.Subscription?.active === true
+              ? currentPlanDetails?.price || '$0'
+              : '$0'
           }
           buttonCta={
-            agencySubscription?.Subscription?.active === true ? 'Change Plan' : 'Get Started'
+            agencySubscription?.Subscription?.active === true
+              ? 'Change Plan'
+              : 'Get Started'
           }
-          highlightDescription='Want to modify your plan? you can do this here. If you have further questions please contact support@plura-app.com'
-          highlightTitle='Plan Options'
+          highlightDescription="Want to modify your plan? You can do this here. If you have
+          further question contact support@plura-app.com"
+          highlightTitle="Plan Options"
           description={
-            agencySubscription?.Subscription?.active === true ? currentPlanDetails?.description || 'Lets get Started' : 'Lets get started! pick a plan that works best for you.'
+            agencySubscription?.Subscription?.active === true
+              ? currentPlanDetails?.description || 'Lets get started'
+              : 'Lets get started! Pick a plan that works best for you.'
           }
-          duration='/ month'
+          duration="/ month"
           features={
-            agencySubscription?.Subscription?.active === true ? currentPlanDetails?.features || [] : currentPlanDetails?.features || pricingCards.find(pricing=> pricing.title === "Starter")?.features || []
+            agencySubscription?.Subscription?.active === true
+              ? currentPlanDetails?.features || []
+              : currentPlanDetails?.features ||
+                pricingCards.find((pricing) => pricing.title === 'Starter')
+                  ?.features ||
+                []
           }
           title={
-            agencySubscription?.Subscription?.active === true ? currentPlanDetails?.title || 'Started' : 'Started'
+            agencySubscription?.Subscription?.active === true
+              ? currentPlanDetails?.title || 'Starter'
+              : 'Starter'
           }
+        />
+        {addOns.data.map((addOn) => (
+          <PricingCard
+            planExists={agencySubscription?.Subscription?.active === true}
+            prices={prices.data}
+            customerId={agencySubscription?.customerId || ''}
+            key={addOn.id}
+            amt={
+              //@ts-ignore
+              addOn.default_price?.unit_amount
+                ? //@ts-ignore
+                  `$${addOn.default_price.unit_amount / 100}`
+                : '$0'
+            }
+            buttonCta="Subscribe"
+            description="Dedicated support line & teams channel for support"
+            duration="/ month"
+            features={[]}
+            title={'24/7 priority support'}
+            highlightTitle="Get support now!"
+            highlightDescription="Get priority support and skip the long long with the click of a button."
           />
+        ))}
       </div>
     </>
   )
