fb1682a2d307925d24f2f03d09f3fa05ff339da5
yacine <yacinomar999@gmail.com>
2024-04-17 08:57:32 +0100

    update nex configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,27 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: [
-          'uploadthing.com',
-          'utfs.io',
-          'img.clerk.com',
-          'subdomain',
-          'files.stripe.com',
+        remotePatterns: [
+          {
+            protocol: 'http' || 'https', // Allow both http and https (optional)
+            hostname: 'uploadthing.com',
+          },
+          {
+            protocol: 'http' || 'https', // Allow both http and https (optional)
+            hostname: 'utfs.io',
+          },
+          {
+            protocol: 'http' || 'https', // Allow both http and https (optional)
+            hostname: 'img.clerk.com',
+          },
+          {
+            protocol: 'http' || 'https', // Allow both http and https (optional)
+            hostname: 'subdomain',
+          },
+          {
+            protocol: 'http' || 'https', // Allow both http and https (optional)
+            hostname: 'files.stripe.com',
+          },
         ],
       },
       reactStrictMode: false,
