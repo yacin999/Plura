b099ba672d94ff724c2762708009b4e83c727cc2
yacine <yacinomar999@gmail.com>
2024-04-28 15:12:16 +0100

    create settings tab in editor funnel page

--- src/app/(main)/subaccount/[subaccountId]/funnels/[funnelId]/editor/[funnelPageId]/_components/funnel-editor-sidebar/index.tsx
+++ src/app/(main)/subaccount/[subaccountId]/funnels/[funnelId]/editor/[funnelPageId]/_components/funnel-editor-sidebar/index.tsx
@@ -1,11 +1,12 @@
 "use client"
 
-import { Sheet, SheetContent } from '@/components/ui/sheet'
-import { Tabs } from '@/components/ui/tabs'
+import { Sheet, SheetContent, SheetDescription, SheetHeader, SheetTitle } from '@/components/ui/sheet'
+import { Tabs, TabsContent } from '@/components/ui/tabs'
 import { useEditor } from '@/providers/editor/editor-provider'
 import clsx from 'clsx'
 import React from 'react'
 import TabList from './tabs'
+import SettingsTab from './tabs/settings-tab'
 
 type Props = {
   subaccountId : string
@@ -26,11 +27,33 @@ const FunnelEditorSidebar = ({subaccountId}: Props) => {
           showX={false}
           side='right'
           className={clsx(
-            'mt-[97px] w-60 z-[80] shadow-none p-0 focus:border-none transition-all overflow-hidden',
+            'mt-[97px] w-16 z-[80] shadow-none p-0 focus:border-none transition-all overflow-hidden',
             {hidden : state.editor.previewMode}
           )}>
             <TabList/>
-          </SheetContent>
+        </SheetContent>
+        <SheetContent
+          showX={false}
+          side={'right'}
+          className={clsx(
+            "mt-[97px] w-80 z-[40] shadow-none p-0 mr-16 bg-background h-full transition-all overflow-hidden",
+            {hidden : state.editor.previewMode}
+          )}
+        >
+          <div className='grid gap-4 h-full pb-36 overflow-scroll'>
+            <TabsContent value='Settings'>
+              <SheetHeader 
+                className='text-left p-6'
+              >
+                <SheetTitle>Styles</SheetTitle>
+                <SheetDescription>
+                  Show your creativity! You can customize every component as you like.
+                </SheetDescription>
+              </SheetHeader>
+              <SettingsTab/>
+            </TabsContent>
+          </div>
+        </SheetContent>
       </Tabs>
     </Sheet>
   )
---
+++ src/app/(main)/subaccount/[subaccountId]/funnels/[funnelId]/editor/[funnelPageId]/_components/funnel-editor-sidebar/tabs/settings-tab.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import React from 'react'
+
+type Props = {}
+
+const SettingsTab = (props: Props) => {
+  return (
+    <div>SettingsTab</div>
+  )
+}
+
+export default SettingsTab
