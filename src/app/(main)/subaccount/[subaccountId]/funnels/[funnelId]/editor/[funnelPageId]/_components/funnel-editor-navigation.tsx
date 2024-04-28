4feb7e9fe1ab4e85f1949bebefd057f66bcf68b4
yacine <yacinomar999@gmail.com>
2024-04-28 10:01:37 +0100

    add tabs to funnnel editor nav

--- src/app/(main)/subaccount/[subaccountId]/funnels/[funnelId]/editor/[funnelPageId]/_components/funnel-editor-navigation.tsx
+++ src/app/(main)/subaccount/[subaccountId]/funnels/[funnelId]/editor/[funnelPageId]/_components/funnel-editor-navigation.tsx
@@ -1,14 +1,17 @@
 'use client'
 
 import { Input } from '@/components/ui/input'
-import { TooltipProvider } from '@/components/ui/tooltip'
-import { useEditor } from '@/providers/editor/editor-provider'
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@/components/ui/tooltip'
+import { toast } from "sonner"
+import { upsertFunnelPage } from '@/lib/queries'
+import { DeviceTypes, useEditor } from '@/providers/editor/editor-provider'
 import { FunnelPage } from '@prisma/client'
 import clsx from 'clsx'
-import { ArrowLeftCircle } from 'lucide-react'
+import { ArrowLeftCircle, Laptop, Smartphone, Tablet } from 'lucide-react'
 import Link from 'next/link'
 import { useRouter } from 'next/navigation'
-import React, { useEffect } from 'react'
+import React, { FocusEventHandler, useEffect } from 'react'
+import { Tabs, TabsList, TabsTrigger } from '@/components/ui/tabs'
 
 type Props = {
     funnelId : string,
@@ -27,8 +30,28 @@ const FunnelEditorNavigation = ({funnelId, funnelPageDetails, subaccountId}: Pro
         })
     }, [funnelPageDetails])
 
-    const handleOnBlurTitleChnage = () => {
+    const handleOnBlurTitleChnage:FocusEventHandler<HTMLInputElement> = async(event) => {
+        if (event?.target.value ===  funnelPageDetails.name) return 
+        if (event.target.value) {   
+            await upsertFunnelPage(
+            subaccountId,
+            {
+                id : funnelPageDetails.id,
+                name : event.target.value,
+                order : funnelPageDetails.order
+            }, 
+            funnelId)
 
+            toast("Success", {
+                description : "Saved funnel page title"
+            })
+            router.refresh()
+        }else {
+            toast("Oppse!", {
+                description : "You need to have a title!"
+            })
+            event.target.value = funnelPageDetails.name
+        }
     }
   return (
     <TooltipProvider>
@@ -56,6 +79,63 @@ const FunnelEditorNavigation = ({funnelId, funnelPageDetails, subaccountId}: Pro
                         Path: /{funnelPageDetails.pathName}
                     </span>
                 </div>
+            </aside>
+            <aside>
+                <Tabs
+                    defaultValue='Desktop'
+                    className='w-fit'
+                    value={state.editor.device}
+                    onValueChange={(value)=>{
+                        dispatch({
+                            type : "CHANGE_DEVICE",
+                            payload : {
+                                device : value as DeviceTypes
+                            }
+                        })
+                    }}
+                >
+                    <TabsList className="grid w-full grid-cols-3 bg-transparent h-fit">
+                        <Tooltip>
+                            <TooltipTrigger>
+                            <TabsTrigger
+                                value="Desktop"
+                                className="data-[state=active]:bg-muted w-10 h-10 p-0"
+                            >
+                                <Laptop/>
+                            </TabsTrigger>
+                            </TooltipTrigger>
+                            <TooltipContent>
+                            <p>Desktop</p>
+                            </TooltipContent>
+                        </Tooltip>
+                        <Tooltip>
+                            <TooltipTrigger>
+                            <TabsTrigger
+                                value="Tablet"
+                                className="w-10 h-10 p-0 data-[state=active]:bg-muted"
+                            >
+                                <Tablet />
+                            </TabsTrigger>
+                            </TooltipTrigger>
+                            <TooltipContent>
+                            <p>Tablet</p>
+                            </TooltipContent>
+                        </Tooltip>
+                        <Tooltip>
+                            <TooltipTrigger>
+                            <TabsTrigger
+                                value="Mobile"
+                                className="w-10 h-10 p-0 data-[state=active]:bg-muted"
+                            >
+                                <Smartphone />
+                            </TabsTrigger>
+                            </TooltipTrigger>
+                            <TooltipContent>
+                            <p>Mobile</p>
+                            </TooltipContent>
+                        </Tooltip>
+                    </TabsList>
+                </Tabs>
             </aside>
         </nav>
     </TooltipProvider>
