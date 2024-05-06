0381e9101af89fa3d3891848ec7ef699324844b2
yacine <yacinomar999@gmail.com>
2024-05-06 18:29:34 +0100

    create funnel pathname page

--- src/app/[domain]/[path]/page.tsx
+++ src/app/[domain]/[path]/page.tsx
@@ -1,31 +1,19 @@
 import FunnelEditor from '@/app/(main)/subaccount/[subaccountId]/funnels/[funnelId]/editor/[funnelPageId]/_components/funnel-editor'
-import { db } from '@/lib/db'
 import { getDomainContent } from '@/lib/queries'
 import EditorProvider from '@/providers/editor/editor-provider'
 import { notFound } from 'next/navigation'
 import React from 'react'
 
-const Page = async ({params} : {params : {domain : string}}) => {
-  console.log("test page pathname :", params?.domain)
+type Props = {}
+
+const Page = async({params }: {params : {domain : string, path : string}}) => {
+
   const domainData = await getDomainContent(params.domain.slice(0, -1))
 
-  if (!domainData) return notFound()
-  
-    const pageData = domainData.FunnelPages.find(page=> !page.pathName)
+  const pageData = domainData?.FunnelPages.find(page => page.pathName === params.path)
 
-    if (!pageData) return notFound()
-
-    await db.funnelPage.update({
-      where : {
-        id : pageData.id
-      },
-      data : {
-        visits : {
-          increment : 1
-        }
-      }
-    })
-  return (  
+  if (!pageData || !domainData) return notFound()
+  return (
     <EditorProvider
       subaccountId={domainData.subAccountId}
       pageDetails={pageData}
--- src/app/[domain]/page.tsx
+++ src/app/[domain]/page.tsx
@@ -1,9 +1,42 @@
+import FunnelEditor from '@/app/(main)/subaccount/[subaccountId]/funnels/[funnelId]/editor/[funnelPageId]/_components/funnel-editor'
+import { db } from '@/lib/db'
+import { getDomainContent } from '@/lib/queries'
+import EditorProvider from '@/providers/editor/editor-provider'
+import { notFound } from 'next/navigation'
 import React from 'react'
 
-const page = () => {
-  return (
-    <div>page</div>
+const Page = async ({params} : {params : {domain : string}}) => {
+  console.log("test page pathname :", params?.domain)
+  const domainData = await getDomainContent(params.domain.slice(0, -1))
+
+  if (!domainData) return notFound()
+  
+    const pageData = domainData.FunnelPages.find(page=> !page.pathName)
+
+    if (!pageData) return notFound()
+
+    await db.funnelPage.update({
+      where : {
+        id : pageData.id
+      },
+      data : {
+        visits : {
+          increment : 1
+        }
+      }
+    })
+  return (  
+    <EditorProvider
+      subaccountId={domainData.subAccountId}
+      pageDetails={pageData}
+      funnelId={pageData.funnelId}
+    >
+      <FunnelEditor 
+        funnelPageId={pageData.id}
+        liveMode={true}
+      />
+    </EditorProvider>
   )
 }
 
-export default page
+export default Page
