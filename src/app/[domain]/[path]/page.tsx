94e8e48bd30619aaf84f3d1f674a7b0eba5a8632
yacine <yacinomar999@gmail.com>
2024-05-06 17:47:54 +0100

    add data and ui to domain

--- src/app/[domain]/[path]/page.tsx
+++ src/app/[domain]/[path]/page.tsx
@@ -1,11 +1,40 @@
+import FunnelEditor from '@/app/(main)/subaccount/[subaccountId]/funnels/[funnelId]/editor/[funnelPageId]/_components/funnel-editor'
+import { db } from '@/lib/db'
 import { getDomainContent } from '@/lib/queries'
+import EditorProvider from '@/providers/editor/editor-provider'
+import { notFound } from 'next/navigation'
 import React from 'react'
 
 const Page = async ({params} : {params : {domain : string}}) => {
   const domainData = await getDomainContent(params.domain.slice(0, -1))
+
+  if (!domainData) return notFound()
   
-  return (
-    <div>subdomain</div>
+    const pageData = domainData.FunnelPages.find(page=> !page.pathName)
+
+    if (!pageData) return notFound()
+
+    await db.funnelPage.update({
+      where : {
+        id : pageData.id
+      },
+      data : {
+        visits : {
+          increment : 1
+        }
+      }
+    })
+  return (  
+    <EditorProvider
+      subaccountId={domainData.subAccountId}
+      pageDetails={pageData}
+      funnelId={pageData.funnelId}
+    >
+      <FunnelEditor 
+        funnelPageId={pageData.id}
+        liveMode={true}
+      />
+    </EditorProvider>
   )
 }
 
