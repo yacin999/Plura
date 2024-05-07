7f1503149aa6ad955e0fcfdce7d3cd81fcd35563
yacine <yacinomar999@gmail.com>
2024-05-07 09:10:30 +0100

    add card to agency dashboard

--- src/app/(main)/agency/[agencyId]/page.tsx
+++ src/app/(main)/agency/[agencyId]/page.tsx
@@ -1,3 +1,4 @@
+import { Card, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
 import { db } from '@/lib/db'
 import { stripe } from '@/lib/stripe'
 import React from 'react'
@@ -39,11 +40,44 @@ const page = async ({params} : {params : {agencyId : string}, searchParams : {co
       created : {gte : startDate, lte : endDate },
       limit : 100
     }, {
-      
+      stripeAccount : agencyDetails.connectAccountId
     })
+    sessions = checkoutSessions.data
+    totalClosedSessions = checkoutSessions.data.filter(session=> session.status === 'complete').map(session => ({
+      ...session,
+      created : new Date().toLocaleDateString(),
+      amount_total : session.amount_total ? session.amount_total /100 : 0
+    }))
+    totalPendingSessions = checkoutSessions.data.filter(session=> session.status === 'open').map(session => ({
+      ...session,
+      created : new Date().toLocaleDateString(),
+      amount_total : session.amount_total ? session.amount_total /100 : 0
+    }))
+    net = +totalClosedSessions
+    .reduce((total, session)=> total + (session.amount_total || 0), 0)
+    .toFixed(2)
+
+    potentialIncome = +totalPendingSessions
+    .reduce((total, session)=> total + (session.amount_total || 0), 0)
+    .toFixed(2)
+
+    closingRate = +((totalClosedSessions.length / checkoutSessions.data.length) * 100).toFixed(2)
   }
   return (
-    <div>{params.agencyId}</div>
+    <div className='relative h-full'>
+      {!agencyDetails.connectAccountId && (
+        <div className='absolute -top-10 -left-10 right-0 bottom-0 z-30 flex items-center justify-center backdrop-blur-md bg-background/50'>
+          <Card>
+            <CardHeader>
+              <CardTitle>Connect Your Stripe</CardTitle>
+              <CardDescription>
+                You need to connect your stripe account to see metrics
+              </CardDescription>
+            </CardHeader>
+          </Card>
+        </div>
+      )}
+    </div>
   )
 }
 
